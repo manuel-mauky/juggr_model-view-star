d3b449881539e323d3fe472d238f00bb808ee28a
Manuel Mauky <manuel.mauky@googlemail.com>
2014-11-29 20:12:50 +0100

    Add business module to access Libary-Server via REST

---
+++ business/src/main/java/de/jug_gr/modeviewstar/business/HalUtil.java
@@ -0,0 +1,16 @@
+package de.jug_gr.modeviewstar.business;
+
+public class HalUtil {
+
+    public static String replaceParam(String href, String param) {
+        return href.replaceFirst("\\{.+\\}", param);
+    }
+
+    public static Integer toInt(Object value) {
+        if (value.getClass().isAssignableFrom(String.class)) {
+            String valueStr = (String) value;
+            return Integer.valueOf(valueStr);
+        }
+        return null;
+    }
+}
---
+++ business/src/main/java/de/jug_gr/modeviewstar/business/LibraryService.java
@@ -0,0 +1,14 @@
+package de.jug_gr.modeviewstar.business;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public interface LibraryService {
+    List<Book> search(String query, Consumer<Error> errorCallback);
+
+    Book showDetails(Book book, Consumer<Error> errorCallback);
+
+    Book lend(String lendTo, Book detailBook, Consumer<Error> errorCallback);
+
+    Book takeBack(Book detailBook, Consumer<Error> errorCallback);
+}
